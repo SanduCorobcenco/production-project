53eda06db2b4ca3ec17784b67446b39a73ab58a1
Sandu Corobcenco <corobcenco.sandu@gmail.com>
2023-04-08 12:14:49 -0700

    fix tests

---
+++ src/shared/lib/tests/renderWithRouter/renderWithRouter.tsx
@@ -0,0 +1,22 @@
+
+import { render } from '@testing-library/react'
+import { type ReactNode } from 'react'
+
+import { MemoryRouter } from 'react-router-dom'
+
+export interface renderWithRouterOptions {
+  route: string
+}
+
+export function renderWithRouter (component: ReactNode, options: renderWithRouterOptions) {
+  const {
+    route = '/'
+  } = options
+
+  return render(
+      <MemoryRouter initialEntries={[route]}>
+          {component}
+      </MemoryRouter>
+
+  )
+}
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,15 +1,16 @@
 import { fireEvent, render, screen } from '@testing-library/react'
 
 import { Sidebar } from './Sidebar'
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender'
 
 describe('Sidebar', () => {
   test('with only first param render', () => {
-    render(<Sidebar/>)
+    componentRender(<Sidebar/>)
     expect(screen.getByTestId('sidebar')).toBeInTheDocument()
   })
 
   test('test toggle', () => {
-    render(<Sidebar/>)
+    componentRender(<Sidebar/>)
     const toggleBtn = screen.getByTestId('sidebar-toggle')
     expect(screen.getByTestId('sidebar')).toBeInTheDocument()
     fireEvent.click(toggleBtn)
