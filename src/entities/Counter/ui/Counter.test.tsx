766429fe66cae6200da7f80740f694460bbdbfca
Sandu Corobcenco <corobcenco.sandu@gmail.com>
2023-05-16 17:36:53 -0700

    tests

--- src/entities/Counter/ui/Counter.test.tsx
+++
@@ -1,5 +0,0 @@
-import { screen } from '@testing-library/react';
-import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
-import { userEvent } from '@storybook/testing-library';
-import { Counter } from './Counter';
-
